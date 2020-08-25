80280056c8877bccce75a8e092c11dc2bc75aa0d
saina-seria <2639497707@qq.com>
2020-08-25 20:53:25 +0800

    zj

--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,7 @@ const ExhSearch = () => import('@/views/exhSearch') //展商搜索列表
 const SurrTraffic = () => import('@/views/surrTraffic') //周边交通
 const CaterHotel = () => import('@/views/caterHotel') //周边餐饮酒店
 const Problem = () => import('@/views/problem') //常见问题
+const newsform = () => import('@/views/newsform')
 // Standard 
 
 
@@ -163,6 +164,10 @@ const routes = [{
             path: 'problem',
             name: '常见问题',
             component: Problem
+        },{
+            path: 'newsform',
+            name: '常见问题',
+            component: newsform
         }]
     },
     
